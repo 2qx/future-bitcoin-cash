c01e4aef5e2934476c6a1c2b1e4a18352957a676
2qx <2qx.git@small.neomailbox.ch>
2024-09-06 08:27:37 -0500

    fix icons

--- packages/lib/src/icons/icons.ts
+++ packages/lib/src/icons/icons.ts
@@ -15,13 +15,13 @@ const colorCodes = [
 const xmlCodes = [
     "000",
     "966424",
-    "ff0000",
+    "f00",
     "ff7500",
-    "ffff00",
-    "00ff00",
-    "0000ff",
-    "ff00ff",
-    "888888",
+    "ff0",
+    "0f0",
+    "00f",
+    "f0f",
+    "888",
     "fff"
 ]
 
@@ -77,7 +77,7 @@ export function getFbchIconSvgUri(n: number, size = 400): string {
     return `data:image/svg+xml,` +
         `%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' style='width:` + size + `px; height: ` + size + `px;'%3E` +
         `%3Cpath d='M 1 1 L 1 15 15 15 15 1 Z' ` +
-        `style='stroke-width: 2px; stroke-linejoin: miter; stroke-linecap: butt; stroke: %23fff; fill:%23` + xmlCodes[places[0]] + `'; ` +
+        `style='stroke-width: 2px; stroke-linejoin: miter; stroke-linecap: butt; stroke: %23fff; fill:%23` + xmlCodes[places[0]] + `; ` +
         `paint-order=stroke'` +
         `%3E%3C/path%3E` +
         `%3Cpath d='M 2 2 L 5 2 5 3 3 3 3 4 4 4 4 5 3 5 3 7 2 7 Z' style='fill: %23fff;'%3E%3C/path%3E` +
