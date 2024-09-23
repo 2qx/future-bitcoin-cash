d7f38ca4080c52b0e3f9669fe72ce38c1d17766c
2qx <2qx@users.noreply.github.com>
2024-09-23 17:09:22 +0000

    deploy: 848c1dc243fa252e5219d4535f01043b2a3d578b

--- _app/immutable/chunks/stores.B7YAYOJ6.js
+++
@@ -1,2 +0,0 @@
-import{s as e}from"./entry.DM3kGPgy.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
-//# sourceMappingURL=stores.B7YAYOJ6.js.map
