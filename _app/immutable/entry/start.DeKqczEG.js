a0e3489bb403d08465edc774b8ef635b275349c2
2qx <2qx@users.noreply.github.com>
2024-10-19 22:20:44 +0000

    deploy: 4bb811b29574ad74d5498fe0482205a8f86180fb

---
+++ _app/immutable/entry/start.DeKqczEG.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.N3dY4cf8.js";export{t as start};
+//# sourceMappingURL=start.DeKqczEG.js.map
--- _app/immutable/entry/start.DmIqLhpF.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/entry.C7_GbugB.js";export{t as start};
-//# sourceMappingURL=start.DmIqLhpF.js.map
