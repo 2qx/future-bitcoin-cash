270be59da82205f8c6e85b6f54d033d0a1454c31
2qx <2qx@users.noreply.github.com>
2024-10-22 17:43:34 +0000

    deploy: eeb286f0ac15d76384bf0cfd74a4d4ba566b3fa5

---
+++ _app/immutable/entry/start.BR8QkQrp.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.BW-6cTTh.js";export{t as start};
+//# sourceMappingURL=start.BR8QkQrp.js.map
--- _app/immutable/entry/start.D7461o7J.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/entry.BhkGdpie.js";export{t as start};
-//# sourceMappingURL=start.D7461o7J.js.map
