26d4ff100c9e2deced371e0ce5e2f4db37d4df79
2qx <2qx@users.noreply.github.com>
2025-02-19 22:11:43 +0000

    deploy: 8d38bc605558baeea5ee99c74fccc53d096f226f

---
+++ _app/immutable/chunks/stores.C67Yi3No.js
@@ -0,0 +1,2 @@
+import{s as e}from"./entry.mzfTzK7v.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
+//# sourceMappingURL=stores.C67Yi3No.js.map
--- _app/immutable/entry/start.CaLUaVWx.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/entry.D20h8SL4.js";export{t as start};
-//# sourceMappingURL=start.CaLUaVWx.js.map
